15df1ae1f0dd2b5a05308ae37de528dcef0e2497
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-09-23 08:30:16 -0400

    chore: minimal reproduction

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -9,5 +9,20 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+  if (req.method !== "GET") {
+    res.setHeader("Allow", ["GET"]);
+    return res.status(405).json({ name: "Method not allowed " });
+  }
+
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  if (!WEBHOOK_SECRET) {
+    return res.status(500).json({ name: "Secret missing" });
+  }
+
+  const requestSecret = req.headers["x-webhook-secret"]?.toString();
+  if (!requestSecret || requestSecret !== WEBHOOK_SECRET) {
+    return res.status(401).json({ name: "Secret does not match" });
+  }
+
+  return res.status(200).json({ name: "Request success" });
 }
